f4b8317e2d90a8486bf9b9b17e4e7004850d3b82
Luka7420 <luka21716@gmail.com>
2025-06-23 17:24:54 +0200

    Added basic mover functionality

--- Source/CryptRaider/Mover.h
+++ Source/CryptRaider/Mover.h
@@ -7,6 +7,7 @@
 #include "Mover.generated.h"
 
 
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class CRYPTRAIDER_API UMover : public UActorComponent
 {
@@ -23,6 +24,15 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+private:
+	UPROPERTY(EditAnywhere)
+	FVector MoveOffset;
 
-		
+	UPROPERTY(EditAnywhere)
+	float MoveTime = 4;
+	
+	UPROPERTY(EditAnywhere)
+	bool ShouldMove = false;
+
+	FVector OriginalLocation;
 };
