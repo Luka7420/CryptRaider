bfe36bf721e8e4a1cb17b7d948e2c3b25c61f08a
Luka7420 <luka21716@gmail.com>
2025-06-24 18:16:18 +0200

    Add debug line drawing and damage reference example to Grabber.cpp

--- Source/CryptRaider/Grabber.cpp
+++ Source/CryptRaider/Grabber.cpp
@@ -3,6 +3,10 @@
 
 #include "Grabber.h"
 
+#include "Engine/World.h"
+
+#include "DrawDebugHelpers.h"
+
 // Sets default values for this component's properties
 UGrabber::UGrabber()
 {
@@ -27,8 +31,22 @@ void UGrabber::BeginPlay()
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
+	// Call the parent class's TickComponent to ensure any inherited behavior runs
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get the current location of this component (the start point for the debug line)
+	FVector Start = GetComponentLocation();
+	// Calculate the end point by moving forward from the start by MaxGrabDistance units
+	FVector End = Start + GetForwardVector() * MaxGrabDistance;
+	// Draw a red debug line in the world from Start to End to visualize the grab range
+	DrawDebugLine(GetWorld(), Start, End, FColor::Red);
+	
+	float Damage = 0;
+	float& DamageRef = Damage;
+	DamageRef = 5; 
+	UE_LOG(LogTemp, Display, TEXT("Damage ref: %f and Damage %f"), DamageRef, Damage); // Log the damage value to the console
+	
+	
 }
+
 
