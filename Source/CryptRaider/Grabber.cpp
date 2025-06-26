5492805a4cb7a79a889ebea24827422de89da7dc
Luka7420 <luka21716@gmail.com>
2025-06-26 18:27:39 +0200

    Implement grab and release functionality with physics handling in Grabber component

--- Source/CryptRaider/Grabber.cpp
+++ Source/CryptRaider/Grabber.cpp
@@ -4,8 +4,8 @@
 #include "Grabber.h"
 
 #include "Engine/World.h"
-
 #include "DrawDebugHelpers.h"
+
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -22,9 +22,6 @@ UGrabber::UGrabber()
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
 }
 
 
@@ -34,38 +31,82 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	// Call the parent class's TickComponent to ensure any inherited behavior runs
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	UPhysicsHandleComponent* PhysicsHandle = GetPhysicsHandle();
+	if(PhysicsHandle == nullptr)
+	{
+		return;
+	}
+	
+	if(PhysicsHandle->GetGrabbedComponent() != nullptr)
+	{
+		FVector TargetLocation = GetComponentLocation() + GetForwardVector() * HoldDistance; // Calculate the target location for the grabbed component, which is the current location of this component plus an offset in the forward direction
+		PhysicsHandle->SetTargetLocationAndRotation(TargetLocation, GetComponentRotation()); // Set the target location and rotation of the physics handle to the calculated target location and the current rotation of this component
+	}
+	
+	
+}
+
+void UGrabber::Grab()
+{
+	UPhysicsHandleComponent* PhysicsHandle = GetPhysicsHandle();
+	if(PhysicsHandle == nullptr)
+	{
+		return;
+	}
+	
+	FHitResult HitResult;
+	bool HasHit = GetGrabbableInReach(HitResult); // Check if there is a grabbable object in reach and store the result in HitResult
+	if(HasHit)
+	{
+		// Grabbing code bellow
+		UPrimitiveComponent* HitComponent = HitResult.GetComponent();
+		HitComponent->WakeAllRigidBodies();
+		PhysicsHandle->GrabComponentAtLocationWithRotation(
+			HitComponent,
+			NAME_None,
+			HitResult.ImpactPoint,
+			GetComponentRotation()
+		);
+	}
 	
 }
 void UGrabber::Release()
 {
-	UE_LOG(LogTemp, Display, TEXT("Released grabber"));
+	UPhysicsHandleComponent* PhysicsHandle = GetPhysicsHandle();
+	if(PhysicsHandle == nullptr)
+	{
+		return;
+	}
+	if(PhysicsHandle->GetGrabbedComponent() != nullptr)
+	{
+		PhysicsHandle->GetGrabbedComponent()->WakeAllRigidBodies();// Wake the grabbed component to ensure it responds to physics immediately
+		PhysicsHandle->ReleaseComponent(); // Release the grabbed component
+	}
 }
-void UGrabber::Grab()
+UPhysicsHandleComponent* UGrabber::GetPhysicsHandle() const
 {
-	
+	UPhysicsHandleComponent* Result = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>(); // Find the PhysicsHandleComponent on the owner actor	
+	if(Result == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Grabber component requires a PhysicsHandleComponent on the owner actor!"));
+	}
+	return Result;
+}
+bool UGrabber::GetGrabbableInReach(FHitResult& OutHitResult) const
+{
 	// Get the current location of this component (the start point for the debug line)
 	FVector Start = GetComponentLocation();
 	// Calculate the end point by moving forward from the start by MaxGrabDistance units
 	FVector End = Start + GetForwardVector() * MaxGrabDistance;
 	// Draw a red debug line in the world from Start to End to visualize the grab range
 	DrawDebugLine(GetWorld(), Start, End, FColor::Red);
+	DrawDebugSphere(GetWorld(), End, 10, 10, FColor::Blue, false, 5);
 	
 	FCollisionShape Sphere = FCollisionShape::MakeSphere(GrabRadius);
-	FHitResult HitResult;
-	bool HasHit = GetWorld()->SweepSingleByChannel(
-		HitResult, 
+	return GetWorld()->SweepSingleByChannel(
+		OutHitResult, 
 		Start, End, 
 		FQuat::Identity, 
 		ECC_GameTraceChannel2, 
-		Sphere
-	);
-	if(HasHit)
-	{
-		AActor* HitActor = HitResult.GetActor();
-		UE_LOG(LogTemp, Display, TEXT("Hit actor: %s"), *HitActor->GetActorNameOrLabel());
-	}
-	else
-	{
-		UE_LOG(LogTemp, Display, TEXT("No actor hit within grab range."));
-	}
+		Sphere);
 }
