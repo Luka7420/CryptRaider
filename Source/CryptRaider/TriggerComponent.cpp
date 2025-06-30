f2ef0e3f152c305a5f62e1397e3e27e476f95298
Luka7420 <luka21716@gmail.com>
2025-06-30 16:12:35 +0200

    Add TriggerComponent functionality with overlap detection and logging

--- Source/CryptRaider/TriggerComponent.cpp
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -3,3 +3,34 @@
 
 #include "TriggerComponent.h"
 
+UTriggerComponent::UTriggerComponent()
+{
+    // Set this component to be initialized when the game starts, and to be ticked every frame.
+    PrimaryComponentTick.bCanEverTick = true;
+
+    
+}
+
+void UTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+    
+}
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) 
+{
+    // Call the parent class's TickComponent to ensure any inherited behavior runs
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    TArray<AActor*> Actors;
+    GetOverlappingActors(Actors);
+
+    
+    for(AActor* Actor : Actors)
+    {
+      if(Actor->ActorHasTag(AcceptableActorTag))
+      {
+        UE_LOG(LogTemp, Display, TEXT("Unlocking"));
+      }
+    }
+    
+}
--- Source/CryptRaider/TriggerComponent.h
+++ Source/CryptRaider/TriggerComponent.h
@@ -9,9 +9,23 @@
 /**
  * 
  */
-UCLASS()
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class CRYPTRAIDER_API UTriggerComponent : public UBoxComponent
 {
 	GENERATED_BODY()
-	
+
+public:
+	UTriggerComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	FName AcceptableActorTag;
 };
